34947e5080683a6573a501de9d2e7f89c69fc3b5
up-to-see-nine-nine-nine-nine <2229781437@qq.com>
2024-05-20 11:32:59 +0800

    新

--- 二叉树/二叉树/Heap.c
+++ 二叉树/二叉树/Heap.c
@@ -23,6 +23,24 @@ void swap(HPDatatype* p1, HPDatatype* p2)
 	*p2 = tmp;
 }
 
+//void AdjustUp(HPDatatype* a, int child)
+//{
+//	int parent = (child - 1) / 2;
+//	while (child > 0)
+//	{
+//		if (a[child] < a[parent])
+//		{
+//			swap(&a[child], &a[parent]);
+//			child = parent;
+//			parent = (child - 1) / 2;
+//		}
+//		else
+//		{
+//			break;
+//		}
+//	}
+//}
+
 void AdjustUp(HPDatatype* a, int child)
 {
 	int parent = (child - 1) / 2;
@@ -64,21 +82,46 @@ void HPPush(HP* php, HPDatatype x)
 	AdjustUp(php->a, php->size - 1);
 }
 
+//void AdjustDown(HPDatatype* a, int n, int parent)
+//{
+//	int child = parent * 2 + 1;
+//	while (child < n)
+//	{
+//		//找出孩子中小的一个
+//		if (child + 1 < n && a[child + 1] < a[child])
+//		{
+//			child++;
+//		}
+//		if (a[child] < a[parent])
+//		{
+//			swap(&a[child], &a[parent]);
+//			parent = child;
+//			child = parent * 2 + 1;
+//		}
+//		else
+//		{
+//			break;
+//		}
+//	}
+//}
+
+//n是元素个数，即最大访问到n-1
 void AdjustDown(HPDatatype* a, int n, int parent)
 {
-	int child = parent * 2 + 1;
+	int child = parent / 2 + 1;
 	while (child < n)
 	{
-		//找出孩子中小的一个
-		if (child + 1 < n && a[child + 1] < a[child])
+		//child+1可能造成数组越界访问
+		//只有一个左孩子就不进行++
+		if (child + 1 < n && a[child] > a[child + 1])
 		{
 			child++;
 		}
-		if (a[child] < a[parent])
+		if (a[parent] > a[child])
 		{
-			swap(&a[child], &a[parent]);
-			parent = child;
-			child = parent * 2 + 1;
+			swap(&a[parent], &a[child]);
+			child = parent;
+			parent = (parent - 1) / 2;
 		}
 		else
 		{
@@ -118,23 +161,27 @@ int HPSize(HP* php)
 void HeapSort(int* a, int n)
 {
 	//倒序：建小堆
-	//向上调整建堆
+	//向上调整建堆:相当于一个个进行插入
 	/*for (int i = 1; i < n; i++)
 	{
 		AdjustUp(a, i);
 	}*/
 
 	//向下调整建堆
+	//从最后一个节点（n-1)的父节点开始
 	for (int i = (n - 1 - 1) / 2; i >= 0; i--)
 	{
 		AdjustDown(a, n, i);
 	}
+
+
+	//n-1是最后一个元素的下标
 	int end = n - 1;
 	while (end > 0)
 	{
+		//此时a[0]最小，放最后，再调整又成新的小堆，a[0]成第二小的
 		swap(&a[0], &a[end]);
 		AdjustDown(a, end, 0);
 		end--;
 	}
-
 }
